632b8a84af58940995800dc1886eaefe4434bb41
727021 <1834544+727021@users.noreply.github.com>
2024-01-07 19:41:47 -0700

    work on collection page

--- app/routes/_app.collection.tsx
+++ app/routes/_app.collection.tsx
@@ -1,6 +1,8 @@
-import type { LoaderFunctionArgs} from '@remix-run/node'
-import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import type { ActionFunctionArgs, LoaderFunctionArgs} from '@remix-run/node'
+import { json, redirect } from '@remix-run/node'
+import { Form, useLoaderData, useNavigation } from '@remix-run/react'
+import { z } from 'zod'
+import { zfd } from 'zod-form-data'
 import { getUser } from '~/services/auth.server'
 import { prisma } from '~/services/db.server'
 
@@ -24,17 +26,68 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   return json({ allExpansions, owned })
 }
 
+const addRemoveSchema = zfd.formData({
+  expansionId: zfd.numeric(z.number().int().positive()),
+  action: zfd.text(z.enum(['add','remove']))
+})
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { action, expansionId } = addRemoveSchema.parse(
+    await request.formData()
+  )
+
+  const user = await getUser(request)
+
+  const expansion = await prisma.expansion.findUnique({
+    where: {
+      id: expansionId
+    }
+  })
+
+  if (!expansion) {
+    throw new Response(null, { status: 404 })
+  }
+
+  if (expansion.defaultOwned) {
+    return new Response(null, { status: 204 })
+  }
+
+  await prisma.user.update({
+    where: {
+      id: user.id
+    },
+    data: {
+      collection: {
+        [action === 'add' ? 'connect' : 'disconnect']: {
+          id: expansion.id
+        }
+      }
+    }
+  })
+
+  return redirect(request.url)
+}
+
 const Collection = () => {
   const data = useLoaderData<typeof loader>()
+  const navigation = useNavigation()
 
   return (
     <div>
       <h1>My Collection</h1>
       <ul>
         {data.allExpansions.map(expansion => (
+          // TODO: pull this out into its own component and use a fetcher or submitter or whatever it's called
           <li key={expansion.id}>
             {data.owned.includes(expansion.id) ? '🟩' : '🟥'}
             {expansion.name}
+            {!expansion.defaultOwned && (
+              <Form method="POST">
+                <input type="hidden" name="expansionId" value={expansion.id} />
+                <input type="hidden" name="action" value={data.owned.includes(expansion.id) ? 'remove' : 'add'} />
+                <button type="submit" disabled={navigation.state !== 'idle' && parseInt(navigation.formData?.get('expansionId')?.toString() ?? '-1', 10) === expansion.id}>{data.owned.includes(expansion.id) ? 'Remove' : 'Add'}</button>
+              </Form>
+            )}
           </li>
         ))}
       </ul>
