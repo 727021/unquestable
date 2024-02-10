9eb7c31c66740875dd27c483ea7c588d75cf6f76
727021 <1834544+727021@users.noreply.github.com>
2024-02-10 16:39:22 -0700

    update footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,15 +1,24 @@
+import { Link } from "@remix-run/react"
+
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-300 text-base-content">
-      {/* Disclaimer copied from https://cards.boardwars.eu/ */}
-      This website is not produced, endorsed, supported, or affiliated with
-      Fantasy Flight Games. The copyrightable portions of Star Wars: Imperial
-      Assault and its expansions are © 2014 - 2016 Fantasy Flight Publishing,
-      Inc. Star Wars, and the characters, items, events and places therein are
-      trademarks or registered trademarks of Lucas Film Limited and are used,
-      under license, by Fantasy Flight Games. The Fantasy Flight Supply and the
-      Fantasy Flight logo are trademarks and/or registered trademarks of Fantasy
-      Flight Publishing, Inc. All Rights Reserved to their respective owners.
+    <footer className="footer footer-center p-10 bg-base-300 text-base-content">
+      <nav className="grid grid-flow-col gap-4">
+        <Link to="#" className="link link-hover">About Us</Link>
+        <Link to="#" className="link link-hover">Terms of Use</Link>
+        <Link to="#" className="link link-hover">Privacy Policy</Link>
+      </nav>
+      <div className="text-left">
+        {/* Disclaimer copied from https://cards.boardwars.eu/ */}
+        This website is not produced, endorsed, supported, or affiliated with
+        Fantasy Flight Games. The copyrightable portions of Star Wars: Imperial
+        Assault and its expansions are © 2014 - 2016 Fantasy Flight Publishing,
+        Inc. Star Wars, and the characters, items, events and places therein are
+        trademarks or registered trademarks of Lucas Film Limited and are used,
+        under license, by Fantasy Flight Games. The Fantasy Flight Supply and the
+        Fantasy Flight logo are trademarks and/or registered trademarks of Fantasy
+        Flight Publishing, Inc. All Rights Reserved to their respective owners.
+      </div>
     </footer>
   )
 }
