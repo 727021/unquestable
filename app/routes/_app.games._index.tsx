3a94e747d3281f520ae1653db8caad460dac2717
727021 <1834544+727021@users.noreply.github.com>
2024-02-10 14:16:40 -0700

    list games on games page

--- app/routes/_app.games._index.tsx
+++ app/routes/_app.games._index.tsx
@@ -7,7 +7,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const user = await getUser(request)
   const games = await prisma.game.findMany({
     where: {
-      owner: user
+      userId: user.id
     },
     include: {
       campaign: true,
@@ -23,12 +23,15 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
         where: {
           resolved: false
         }
-      }
+      },
+      imperialPlayer: true
     },
     orderBy: {
       started: 'desc'
     }
   })
+
+  console.log(games)
 
   return json({ games })
 }
@@ -37,11 +40,45 @@ const Games = () => {
   const { games } = useLoaderData<typeof loader>()
 
   return (
-    <div>
-      <h1>Games</h1>
+    <div className="prose max-w-full">
+      <h1 className="m-0">Games</h1>
       {games.length ? (
         <>
-          <Link to="/games/new">New Game</Link>
+          <Link to="/games/new" className="link-hover">
+            New Game
+          </Link>
+          <div className="flex gap-2 p-2 flex-wrap max-w-full">
+            {games.map((game) => (
+              <Link
+                key={game.id}
+                to={`/games/${game.id}`}
+                className="card card-compact card-bordered no-underline shadow-md hover:shadow-lg w-64 max-w-full"
+              >
+                <div className="card-body w-full">
+                  <h2 className="card-title m-0">{game.name}</h2>
+                  <div className="flex justify-between">
+                    <span>{game.campaign.name}</span>
+                    <span>{new Date(game.started).toDateString()}</span>
+                  </div>
+                  <dl className="m-0">
+                    <dt className="m-0">Heroes</dt>
+                    {game.rebelPlayers.map((p) => (
+                      <dd key={p.id}>
+                        {p.hero.name}
+                        {p.name && ` (${p.name})`}
+                      </dd>
+                    ))}
+                    {game.imperialPlayer?.name && (
+                      <>
+                        <dt className="m-0">Imperial</dt>
+                        <dd>{game.imperialPlayer.name}</dd>
+                      </>
+                    )}
+                  </dl>
+                </div>
+              </Link>
+            ))}
+          </div>
         </>
       ) : (
         <>
